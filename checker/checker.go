fac6a6d48398a469be927c74c60fcfb6fed3ea21
moomien <alexspencer980@gmail.com>
2026-05-07 01:48:54 +0300

    реализовал отправку запроса к апи Arkham на получение твиттера связанного с кошем

---
+++ checker/checker.go
@@ -0,0 +1,50 @@
+package checker
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/tidwall/gjson"
+)
+
+func FetchTwitterWithRetry(client *resty.Client, address string) string {
+	url := arkhamURL(address)
+
+	exponenntialBackoff := []int{1, 2, 4, 8, 16}
+	for attempt := 0; attempt <= len(exponenntialBackoff); attempt++ {
+		resp, err := newArkhamRequest(client).Get(url)
+		if err != nil {
+			slog.Error(fmt.Sprintf("Failed to request, retrying %d", attempt), "err", err)
+			continue
+		}
+
+		if resp.StatusCode() == 200 {
+			twitter := gjson.Get(resp.String(), "arkhamEntity.twitter")
+			if !twitter.Exists() {
+				twitter := "Nope"
+				return twitter
+			}
+			return twitter.String()
+		}
+
+		slog.Error("Arkham api returned bad status", "status", resp.StatusCode(), "attempt", attempt+1)
+	}
+
+	return "Failed to fetch"
+}
+
+func newArkhamRequest(client *resty.Client) *resty.Request {
+	cookie := os.Getenv("cookie")
+	return client.NewRequest().SetHeader("Connection", "keep-alive").SetHeader("Accept", "application/json").
+		SetHeader("Accept-language", "en-US,en;q=0.9").SetHeader("Origin", "https://intel.arkm.com").
+		SetHeader("pragma", "no-cache").SetHeader("priority", "u=1, i").SetHeader("Referer", "https://intel.arkm.com/").
+		SetHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36").
+		SetHeader("Cookie", cookie)
+}
+
+func arkhamURL(address string) string {
+	return "https://api.arkm.com/intelligence/address/" + strings.TrimSpace(address)
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module arkham_checker
+
+go 1.25.5
+
+require (
+	github.com/go-resty/resty/v2 v2.17.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+)
